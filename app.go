512ef75979680dcdc49199fff8acd7e43f76af92
universalmind <universalmind.candy@gmail.com>
2017-09-05 09:59:34 -0500

    finished product

--- app.go
+++ app.go
@@ -1,6 +1,5 @@
 package httpserver
 import ("net/http")
-
 func init() {
   assets := http.StripPrefix("/", http.FileServer(http.Dir("static/")))
   http.Handle("/", assets)
--- static/src/app.js
+++ static/src/app.js
@@ -1,9 +1,3 @@
 angular.module('myApp', ['Shapes','ShapeRenderer'])
-  .controller('main', ($scope) =>{
-    $scope.createShape = () => {
-      if($scope.shape ==='Square') $scope.shapeList.push(new Square($scope.size))
-      if($scope.shape ==='Circle') $scope.shapeList.push(new Circle($scope.size))
-    }
-  })
 
 
