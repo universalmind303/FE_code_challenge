39868f8283dadaf0cf2b04383796ff100acb9992
universalmind <universalmind.candy@gmail.com>
2017-09-25 20:43:50 -0500

    xxx

--- app.go
+++ app.go
@@ -1,6 +1,9 @@
 package main
-import ("net/http")
-func init() {
+import (
+"net/http"
+"log"
+)
+func main() {
   assets := http.StripPrefix("/", http.FileServer(http.Dir("static/")))
   http.Handle("/", assets)
   log.Fatal(http.ListenAndServe(":8080", nil))
